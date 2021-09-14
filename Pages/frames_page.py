f984e231f721b2b4524542bc82357fac0553bc83
Terry <terrym@trustpeers.com>
2021-09-14 15:57:24 +0300

    6. test iframe

--- Pages/frames_page.py
+++ Pages/frames_page.py
@@ -1,3 +1,27 @@
+import time
+
+from selenium.webdriver.common.by import By
+from HtmlElement import HTMLElement
+
+
+class FramesPageSelectors:
+    IfremeLink = "#content > div > ul > li:nth-child(2) > a"
+    IframeInput = "#tinymce > p"
+
+
 class FramesPage:
     def __init__(self, driver):
-        pass
+        self.driver = driver
+        self._iframe_link = HTMLElement(self.driver, by=By.CSS_SELECTOR, locator_value=FramesPageSelectors.IfremeLink)
+
+    def enter_iframe_page(self):
+        self._iframe_link.click()
+
+    def send_text(self, txt):
+        self.enter_iframe_page()
+        iframe = self.driver.find_elements_by_tag_name('iframe')
+        self.driver.switch_to.frame(iframe)
+        content_box = HTMLElement(self.driver, by=By.CSS_SELECTOR, locator_value=FramesPageSelectors.IframeInput)
+        content_box.input_text(txt=txt)
+        time.sleep(2)
+        self.driver.switch_to.default_content()
--- Test/capitolis_tests.py
+++ Test/capitolis_tests.py
@@ -7,3 +7,16 @@ class CapitolisTests(BasicTest):
         checkboxes_page = BasePage(browser).click_checkboxes()
         checkboxes_page.toggle_checkboxes()
         assert True
+
+    def test_basic_auth(self, browser):
+        pass
+
+    def test_iframe(self, browser):
+        frames_page = BasePage(browser).click_frames()
+        frames_page.send_text("Terry Meir")
+
+    def test_dynamic_loading(self, browser):
+        pass
+
+    def test_jquery_menu(self, browser):
+        pass
